c88a48e1652d3047c4bce28de9b6b4891e8f98e5
luofu <mqa010225@163.com>
2024-10-10 16:08:46 +0800

    <day1>(<base>): <change>
    input function

--- day1/__init__.py
+++ day1/__init__.py
@@ -3,3 +3,7 @@ Day 1 - Python Print Function
 The function is declared like this:
 print('what to print')
 """)
+
+print("hello " + input("What's your name?") + "！")
+
+print(len(input("What's your name?")))
