c295bb03d3b87187bb511c22bc37ab9c6554ddb4
luofu <mqa010225@163.com>
2024-10-14 21:19:28 +0800

    <feat>(<day02>): <date type>
    date type

---
+++ day02/__init__.py
@@ -0,0 +1,13 @@
+print("hello"[1])
+
+print("hello" + " world!")
+
+print("Python Data Type : Float String Int Boolean")
+
+type("hello")
+
+# exercise 01
+
+number = input("type a two digit number:")
+print(int(number[0]) + int(number[1]))
+
